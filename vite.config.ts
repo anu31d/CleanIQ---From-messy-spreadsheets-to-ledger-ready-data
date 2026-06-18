b68f62f4787939d172424b6468e1dfc0ba18d16a
Anuska Dasgupta <165494478+anu31d@users.noreply.github.com>
2026-06-18 19:21:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,19 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-  return {
-    base: '/CLEANIQ/',
+export default defineConfig({
+  base: '/CleanIQ---From-messy-spreadsheets-to-ledger-ready-data/',
 
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
+  plugins: [react(), tailwindcss()],
 
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
+  },
 
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-  };
+  server: {
+    hmr: process.env.DISABLE_HMR !== 'true',
+    watch: process.env.DISABLE_HMR === 'true' ? null : {},
+  },
 });
